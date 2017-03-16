0917f0d17cb53c2b31cad9d1a1914bd2509dc6a1
Su Yijia <crystalremnet@gmail.com>
2017-03-16 14:26:02 +0800

    Init Commit

---
+++ MobileVA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileVA
+//
+//  Created by Su Yijia on 3/16/17.
+//  Copyright © 2017 Su Yijia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MobileVA/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MobileVA
+//
+//  Created by Su Yijia on 3/16/17.
+//  Copyright © 2017 Su Yijia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
